2704b7c4bca1219a3cae3c38658e8d1924382868
Nate Dwyer <nate9799@gmail.com>
2019-02-08 14:54:39 -0500

    Adds heatmap and example with two sellers.

--- Model_v2.0.py
+++ Model_v2.0.py
@@ -1,11 +1,29 @@
 # Technical debt from pricing model. can speed up by factor of n
 # also, occasional errors in negative prices
 # improved pricing model in 1.2
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.externals import joblib as jl
 
 
+
+
+# CONSTANTS 
+NUM_BUYERS = 100
+NUM_SELLERS = 20
+#A_BUYER_LOC = np.random.rand(NUM_BUYERS)
+#A_SELLER_LOC = np.random.rand(NUM_SELLERS)
+COST = 1
+GAMMA = 0
+SLOPE = 1
+ENDOWMENT = 2
+
+a_seller_loc = [.2, .7]
+a_buyer_loc = [.2, .2, .2, .2, .2, .7, .7, .7, .7, .7]
+SELLER_PRICE = 1
+SELLER_QUANTITY = 10
 
 #####################
 #####################
@@ -66,6 +84,8 @@ def find_quantity_sold(a_quantity, m_price_rel):
     for col in m_price_rel.T:
         a_quantity_bought = find_bundle(a_quantity_unsold, col)
         a_quantity_unsold = a_quantity_unsold - a_quantity_bought
+        if sum(a_quantity_unsold) <= 0:
+            return a_quantity
     a_quantity_sold = a_quantity - a_quantity_unsold
     return a_quantity_sold
 
@@ -79,10 +99,21 @@ def find_profit(a_quantity, a_price, m_tax, cost):
     a_quantity_sold = find_quantity_sold(a_quantity, m_price_rel)
     a_revenue = a_price * a_quantity_sold
     a_cost = cost * a_quantity
-    a_quantity_unsold = a_quantity - a_quantity_sold
     a_profit = a_revenue - a_cost
     return a_profit
 
+def find_profit_2(seller0_price, seller0_quantity, seller1_price, seller1_quantity, m_tax, cost):
+    a_profit = find_profit(np.array([seller0_quantity, seller1_quantity]),
+            np.array([seller0_price, seller1_price]), m_tax, cost)
+    return a_profit[1]
+
+
+def heatmap(a_price_range, a_quantity_range, seller0_price, seller0_quantity, m_tax, cost):
+    heat = [[find_profit_2(seller0_price, seller0_quantity, price_tmp,
+        quantity_tmp, m_tax, cost) for quantity_tmp in a_quantity_range]
+        for price_tmp in reversed(a_price_range)]
+    return(sns.heatmap(heat))
+    
 
 ##################
 ##################
@@ -90,12 +121,12 @@ def find_profit(a_quantity, a_price, m_tax, cost):
 ##################
 ##################
 
-# CONSTANTS 
-NUM_BUYERS = 100
-NUM_SELLERS = 20
-A_BUYER_LOC = np.random.rand(NUM_BUYERS)
-A_SELLER_LOC = np.random.rand(NUM_SELLERS_START)
-GAMMA = 0
-SLOPE = 1
-ENDOWMENT = 2
+print(np.arange(.1,2,.1))
+plt.figure(figsize=(20, 20))
+m_tax = get_m_tax(a_buyer_loc, a_seller_loc, GAMMA)
 
+ax_heat = heatmap(np.arange(.1,2,.1), np.arange(0,10, 1), SELLER_PRICE, SELLER_QUANTITY, m_tax, COST)
+
+ax_heat.set(ylabel = 'price', xlabel = 'quantity')
+ax_heat.set_yticklabels(reversed(np.arange(.1,2,.1)), rotation=0)
+plt.show()
