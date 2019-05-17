2f70f8241974381d1d917698358d7ab5b77f0c08
Nate <nate9799@gmail.com>
2019-05-16 23:06:36 -0400

    Fixed bug

--- code/cluster_execution_turn.sh
+++ code/cluster_execution_turn.sh
@@ -1,4 +1,4 @@
 
 module load new gcc/4.8.2 python/2.7.14
 
-bsub -n 6 -W 24:00 -R "rusage[mem=6000]" -J "Cournot_Turn" "python -W ignore model_turn.py"
+bsub -n 6 -W 24:00 -R "rusage[mem=6000]" -J "Cournot_Turn" "python -W ignore turn_model.py"
