ce823506aebcc473c852a9e084f2e328b5c7cc6e
Sandro Lera <slera@eu-login-05-ng.euler.ethz.ch>
2019-06-11 18:22:58 +0200

    fixed time allocation in bash

--- code/cluster_execution_turn.sh
+++ code/cluster_execution_turn.sh
@@ -1,4 +1,4 @@
 
 module load new gcc/4.8.2 python/2.7.14
 
-bsub -n 1 -W 24:00 -R "rusage[mem=1000]" -J "Cournot_Turn [1-20]" "python -W ignore turn_model.py \$LSB_JOBINDEX "
+bsub -n 1 -W 4:00 -R "rusage[mem=1000]" -J "Cournot_Turn [1-20]" "python -W ignore turn_model.py \$LSB_JOBINDEX "
