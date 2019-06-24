3dce7071b602f42db3b11341fe38de28cea0580f
Sandro Lera <slera@eu-login-01-ng.euler.ethz.ch>
2019-06-24 18:33:00 +0200

    some updates for many functions, but not everything

--- code/turn_model.py
+++ code/turn_model.py
@@ -8,7 +8,8 @@ import numpy as np
 def new_a_cost(a_cost, a_profit):
     a_profit_ratio = a_profit/np.sum(a_profit)
     a_cost_decrease_ratio = 1 - .05*a_profit_ratio
-    ret = a_cost_decrease*a_cost
+    ret = a_cost_decrease_ratio*a_cost
+    print(ret)
     return ret
 
 def get_settings_from_dic_ret(dic_ret):
@@ -18,7 +19,7 @@ def get_settings_from_dic_ret(dic_ret):
     num_buyers = dic_ret['num_buyers']
     ret = { 'num_sellers' : dic_ret['num_sellers'],
             'num_buyers' : num_buyers,
-            'a_strat_quantity' : np.arange(0, 100*num_buyers, 40),
+            'a_strat_quantity' : np.arange(0, 100*num_buyers, 21),
             'a_seller_loc' : dic_ret['a_seller_loc'],
             'a_buyer_loc' : dic_ret['a_buyer_loc'],
             'a_cost' : dic_ret['a_cost'],
@@ -80,23 +81,28 @@ def process(d_settings, num_loops):
     folder1 = '/home/nate/Documents/abmcournotmodel/code/output/data/'
     folder2 = '/cluster/home/slera//abmcournotmodel/code/output/data/'
     folder  = folder1 if os.path.exists(folder1) else folder2
-    fn = folder + 'turn_gamma=' + str(round(d_settings['gamma'],3)) + '.pickle'
+    fn = folder + 'alt_adv_fast_turn_gamma={}_scalar_tax={}_endow={}.pickle'.format(round(d_settings['gamma'],3), d_settings['scalar_tax'], d_settings['endowment'])
     jl.dump(d_turns, fn)
 
 if __name__ == "__main__":
     i = int(sys.argv[1]) - 1
     gamma = i/10.
+    scalar_tax = 1.0
+    endowment = 200
     num_sellers = 2
-    num_buyers = 13
+    num_buyers = 12
+    np.random.seed(17)
+    a_seller_loc = np.random.uniform(low=0, high=1, size=num_sellers)
+    a_seller_loc = np.random.uniform(low=0, high=1, size=num_sellers)
     d_settings = { 'num_sellers' : num_sellers,
             'num_buyers' : num_buyers,
             'a_strat_quantity' : np.linspace(0, num_buyers*100, 21),
             'a_seller_loc' : np.linspace(start=0, stop=1, num=num_sellers, endpoint=False),
             'a_buyer_loc'  : np.linspace(start=0, stop=.999, num=num_buyers,  endpoint=False),
-            'a_cost' : [100, 100],
+            'a_cost' : np.array([99, 100]),
             'gamma' : gamma,
-            'scalar_tax' : .05,
-            'endowment' : 200,
+            'scalar_tax' : scalar_tax,
+            'endowment' : endowment,
             'mean_cost' : 100,
             'cost_ratio' : 1} 
     process(d_settings, 20)
