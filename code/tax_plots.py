972870f782065f4a18a8606b5825e5cb7b59d460
Nate Dwyer <nate9799@gmail.com>
2019-09-11 14:30:07 -0400

    Fixed a lot of plotting things.

--- code/tax_plots.py
+++ code/tax_plots.py
@@ -10,72 +10,65 @@ from matplotlib import ticker as tk
 from pprint     import pprint as pp
 from sklearn.externals import joblib as jl
 from itertools  import product,combinations
-from plot_util import customaxis
+from plot_util  import customaxis
+from time_plots import dic_from_a_dic
 
 
 # plot the profit and quantity sold for each seller 
 ############################################################
 def make_ax_profit_quantity(pd_profit, pd_quantity_sold, a_marker, a_color,
-        num_sellers, gamma, fontsize=20, x_ticks=None, ax=None):
-    # FIXME: x_ticks
+        num_timesteps, num_sellers, fontsize=20, ax=None):
     color = a_color[0]
     pd_data = pd_profit
-    if x_ticks is None:
-        x_ticks = range(len(pd_data))
-    x_ticks = np.round(x_ticks, 2)
     for i in range(num_sellers):
-        ax.plot(x_ticks, pd_data[i], '-', marker=a_marker[i],
+        ax.plot(range(len(pd_data)), pd_data[i], '-', marker=a_marker[i],
                 markerfacecolor='none', color=color, label = 'Firm %d'%(i+1))
-    ax.set_xlim(min(x_ticks), max(x_ticks))
-    ax.set_xticks(x_ticks)
+    ax.set_xlim(0, num_timesteps)
+    ax.set_xticks(np.arange(0, num_timesteps+1))
     customaxis(ax = ax, position = 'left', color = color, label = 'Profit',
             scale = 'linear', size = fontsize, full_nrs = False, location = 0.0)
     ax.get_xaxis().set_major_formatter(tk.FuncFormatter(lambda x, p:
-        format(x,',')))
+        format(int(x),',')))
     ax.legend()
     leg = ax.get_legend()
     (leg.legendHandles[i].set_color('black') for i in range(num_sellers))
-    ax.set_title("$\gamma$ = {} and Firm 1's initial cost is 99\% of Firm 2's".format(gamma),
+# FIXME: title
+    #ax.set_title("Firm 1's initial cost is 99\% of Firm 2's".format(gamma),
             fontsize=fontsize +5)
 # Quantity
     axt = ax.twinx()
     color = a_color[1]
     pd_data = pd_quantity_sold
     for i in range(num_sellers):
-        axt.plot(x_ticks, pd_data[i], '-', marker=a_marker[i],
+        axt.plot(range(len(pd_data)), pd_data[i], '-', marker=a_marker[i],
                 markerfacecolor='none', color=color)
-    axt.grid(False)
+    axt.grid('False')
     customaxis(ax = axt, position = 'right', color = color, label = 'Quantity',
             scale = 'linear', size = fontsize, full_nrs = False, location = 1.0)
 
 # plot the cost and price
 ############################################################
 def make_ax_cost_price(pd_cost, pd_price, pd_cournot, a_marker, a_color,
-        num_sellers, gamma, fontsize = 20, x_ticks=None, ax = None):
+        num_timesteps, num_sellers, fontsize = 20, ax = None):
+    ax.set_xlim(0, num_timesteps)
 # for the left y axis of axis:
     pd_data = pd_price
     color = a_color[0]
-    if x_ticks is None:
-        x_ticks = range(len(pd_data))
     for i in range(num_sellers):
-        ax.plot(x_ticks, pd_data[i], color=color, marker = a_marker[i],
+        ax.plot(range(len(pd_data)), pd_data[i], color=color, marker = a_marker[i],
                 markerfacecolor='none', label='')
-    if gamma == 0:
-        ax.plot(x_ticks, pd_cournot, color=color, linestyle='dashed',
-                label='Theoretical Cournot Price')
-        ax.legend(loc='lower left')
     ax.spines['left'].set_color(color)
     ax.tick_params(axis='y', color=color)
     [i.set_color(color) for i in ax.get_yticklabels()]
     ax.yaxis.set_label_position("left")
     ax.set_ylabel('Price', color=color, fontsize=fontsize)
-    ax.set_xlim(min(x_ticks), max(x_ticks))
+    ax.set_xlim(0, num_timesteps)
 # for the 'right' axis it is similar
     axt   = ax.twinx()
     pd_data = pd_cost
     color = a_color[1]
     for i in range(num_sellers):
-        axt.plot(x_ticks, pd_data[i], marker= a_marker[i],
+        axt.plot(range(len(pd_data)), pd_data[i], marker= a_marker[i],
                 markerfacecolor='none', color=color)
     axt.spines['right'].set_color(color)
     axt.tick_params(axis='y', color=color)
@@ -86,13 +79,11 @@ def make_ax_cost_price(pd_cost, pd_price, pd_cournot, a_marker, a_color,
     axt.grid(False)
     return ax
 
-def make_column(fn, ax_profit_quantity, ax_cost_price, x_ticks_name=None):
+def make_column(fn, ax_profit_quantity, ax_cost_price):
     print(fn)
     d_load = jl.load(fn)
 # Data from d_load
 ##########################################################
-    scalar_tax          = d_load['scalar_tax'][0]
-    gamma               = d_load['gamma'][0]
     endowment           = d_load['endowment'][0]
     num_sellers         = d_load['num_sellers'][0]
     num_buyers          = d_load['num_buyers'][0]
@@ -100,18 +91,13 @@ def make_column(fn, ax_profit_quantity, ax_cost_price, x_ticks_name=None):
     a_seller_pos        = d_load['a_seller_loc'][0]
     m_tax               = d_load['m_tax'][0]
     a_m_quantity_bought = d_load['m_quantity_bought']
+    pd_scalar_tax       = pd.DataFrame(d_load['scalar_tax'])
+    pd_gamma            = pd.DataFrame(d_load['gamma'])
     pd_quantity         = pd.DataFrame(d_load['a_quantity_nash'])
     pd_quantity_sold    = pd.DataFrame(d_load['a_quantity_sold'])
     pd_price            = pd.DataFrame(d_load['a_price_nash'])
     pd_cost             = pd.DataFrame(np.array(d_load['a_cost']))
     pd_profit           = pd.DataFrame(d_load['a_profit'])
-# x_ticks stuff
-    x_ticks = range(len(pd_profit))
-    if x_ticks_name is not None:
-        print(x_ticks_name)
-        x_ticks = d_load[x_ticks_name]
-    x_ticks = np.round(x_ticks, 3)
-    print(x_ticks)
 # Data from calculations
     pd_num_buyers_per_seller = pd.DataFrame([np.count_nonzero(m_quant, axis=1) for
         m_quant in a_m_quantity_bought])
@@ -127,19 +113,17 @@ def make_column(fn, ax_profit_quantity, ax_cost_price, x_ticks_name=None):
     a_color = sb.color_palette("deep", num_sellers+4)
 # Plots Here
     make_ax_profit_quantity(pd_profit, pd_quantity_sold, a_marker,
-            a_color[0:2], num_sellers, gamma, x_ticks=x_ticks,
+            a_color[0:2], num_timesteps, num_sellers, gamma,
             ax=ax_profit_quantity)
-    print(5)
     make_ax_cost_price(pd_cost, pd_price, pd_cournot, a_marker, a_color[2:4],
-            num_sellers, gamma, x_ticks=x_ticks, ax=ax_cost_price)
+            num_timesteps, num_sellers, gamma, ax=ax_cost_price)
 
-def write_time_plot_from_file(fn, fn_out, folder = None):
-    print(fn)
+def write_time_plot_from_file(a_fn, folder = None):
     sb.set_style("darkgrid")
 # set figure size
 ##########################################################
     nrow            = 2
-    ncol            = 1
+    ncol            = 2
     width_scale     = 12
     height_scale    = 4
     figsize         = (width_scale*ncol,height_scale*nrow)
@@ -151,23 +135,27 @@ def write_time_plot_from_file(fn, fn_out, folder = None):
     plt.rc('text', usetex=True)
 # set all axes instances
 ##########################################################
-    ax_profit_quantity  = plt.subplot2grid((nrow,ncol), (0,0))
-    ax_cost_price       = plt.subplot2grid((nrow,ncol), (1,0), sharex=ax_profit_quantity)
+    ax_profit_quantity1 = plt.subplot2grid((nrow,ncol), (0,0))
+    ax_profit_quantity2 = plt.subplot2grid((nrow,ncol), (0,1))
+    ax_cost_price1      = plt.subplot2grid((nrow,ncol), (1,0), sharex=ax_profit_quantity1)
+    ax_cost_price2      = plt.subplot2grid((nrow,ncol), (1,1), sharex=ax_profit_quantity2)
 # annotate
 ##########################################################
     a_ax = fig.get_axes()
-    a_annote = ['(a)','(b)','(c)','(d)','(e)','(f)','(g)','(h)']
+    a_annote = ['(a)','(b)','(c)','(d)']
     for (ax, annote) in zip(a_ax, a_annote[:len(a_ax)]):
         ax.annotate(annote,
-                xy          =  (-0.12, 0.96),
+                xy          = (-0.12, 0.96),
                 xycoords    = 'axes fraction',
                 fontsize    =  12,
                 ha          = 'left',
                 va          = 'top' )
-    make_column(fn, ax_profit_quantity, ax_cost_price, 'scalar_tax')
+    make_column(a_fn[0], ax_profit_quantity1, ax_cost_price1)
+    make_column(a_fn[1], ax_profit_quantity2, ax_cost_price2)
+
 # write figure to the output 
 ############################################################
-    fn_out = 'plot_' + fn_out
+    fn_out = "timestep"
     out_folder = './output/plots/'
     if not os.path.exists(out_folder): os.makedirs(out_folder)
     plt.savefig(out_folder + fn_out + '.png', bbox_inches='tight')
@@ -182,12 +170,13 @@ def write_time_plot_from_file(fn, fn_out, folder = None):
 ##############
 
 if __name__ == "__main__":
-    a_fn_out = np.array(['turn_gamma=1.0_endow=120.0_taxmethod=cardinal_seed={}.pickle'.format(i)
-        for i in np.arange(0,69,17)])
+    a_fn = np.array(['alt_adv_fast_turn_gamma=0.0_scalar_tax=1.0_endow=200.pickle'.format(i)
+        for i in np.arange(125,205,5)])
     folder1 = '/home/nate/Documents/abmcournotmodel/code/output/data/'
     folder2 = '/cluster/home/slera//abmcournotmodel/code/output/data/'
-    folder3 = "C:/Users/CAREBEARSTARE3_USER/Documents/WORK/MITInternship/ModelWithSandro/abmcournotmodel/code/output/data/"
-    folder = folder3
+    folder  = "C:/Users/CAREBEARSTARE3_USER/Documents/WORK/MITInternship/ModelWithSandro/abmcournotmodel/code/output/data/results/"
     a_fn = glob.glob(folder + "turn*")
-    [write_time_plot_from_file(a_fn[i], a_fn_out[i]) for i in range(len(a_fn))]
+    a_dic = [jl.load(fn) for fn in a_fn]
+    dic = dic_from_a_dic(a_dic)
+    write_time_plot_from_file(dic)
 
